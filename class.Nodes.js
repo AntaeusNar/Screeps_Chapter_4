df0abc0e7b2531c025d5e673158d990228ce925d
AntaeusNar <jedidiah.h.wallace@gmail.com>
2024-09-30 18:50:27 -0700

    Started Nodes

---
+++ class.Nodes.js
@@ -0,0 +1,10 @@
+/** Nodes are the building blocks of transportation routes for the Omni-Union
+ * 
+ * The idea is that a Node exists as a base class that is extendable under different 
+ * situations for different uses.
+ * 
+ * Node types:
+ * Container Miner (Source/Mineral/Deposit)
+ * Link Miner (Source)
+ *  
+ */
