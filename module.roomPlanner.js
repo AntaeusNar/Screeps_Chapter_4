ede4c9c4a68ce8442e67d3a9686b9ed693064b6a
AntaeusNar <jedidiah.h.wallace@gmail.com>
2024-10-08 20:29:16 -0700

    started room planner

---
+++ module.roomPlanner.js
@@ -0,0 +1,6 @@
+/** This Module will allow for the automatic placement of buildings.
+ * Phase one will be the construction of the central hub around the spawn from level 2 to 8 of the room controller.
+ * Phase two will include room level defenses, including walls, ramparts, and towers.
+ * Phase three will include additional advanced units
+ */
+
